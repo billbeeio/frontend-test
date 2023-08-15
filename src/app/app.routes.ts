6952815d7a85f2aff104c0f39e43656769c512da
jan.hommels@gmx.de <jan.hommels@gmx.de>
2023-08-15 11:34:03 +0200

    feat(client): Add Start and Hive Pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import {Routes} from "@angular/router";
+import {HiveListComponent} from "./hive/list/hive-list.component";
+import {StartComponent} from "./start/start.component";
 
-export const ROUTES: Routes = [];
+export const ROUTES: Routes = [
+    {
+        path: "",
+        pathMatch: "full",
+        component: StartComponent,
+    },
+    {
+        path: "hive",
+        component: HiveListComponent,
+    },
+];
---
+++ src/app/hive/list/hive-list.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: "app-hive-list",
+    templateUrl: "./hive-list.component.html",
+    styleUrls: ["./hive-list.component.scss"],
+    standalone: true,
+    imports: [],
+})
+export class HiveListComponent {}
