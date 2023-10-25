691138e216d5c849b6923573a7f80da91325fe6e
jan.hommels@gmx.de <jan.hommels@gmx.de>
2023-10-25 15:12:42 +0200

    Add functionality to hi....deleted 🐛🐛🐛🐛

--- src/app/hive/list/hive-list.component.ts
+++ src/app/hive/list/hive-list.component.ts
@@ -1,10 +1,30 @@
-import {Component} from "@angular/core";
+import {Component, Signal, inject, signal} from "@angular/core";
+import {MatButtonModule} from "@angular/material/button";
+import {MatDialog, MatDialogModule} from "@angular/material/dialog";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {AddDialogComponent} from "../add-dialog/add-dialog.component";
+import {BeeCardComponent} from "../bee-card/bee-card.component";
+import {Bee, BeeAmount} from "../hive.model";
+import {HiveService} from "../hive.service";
 
 @Component({
     selector: "app-hive-list",
     templateUrl: "./hive-list.component.html",
     styleUrls: ["./hive-list.component.scss"],
     standalone: true,
-    imports: [],
+    imports: [MatButtonModule, BeeCardComponent, MatProgressSpinnerModule, MatDialogModule],
+    providers: [HiveService],
 })
-export class HiveListComponent {}
+export class HiveListComponent {
+  private readonly matDialog = inject(MatDialog);
+
+  readonly bees: Signal<Bee[]> = signal([]); //.....deleted 🐛🐛🐛🐛
+
+  //.....deleted 🐛🐛🐛🐛
+
+  add(bees: Bee[]) {
+    this.matDialog
+        .open<AddDialogComponent, Bee[], BeeAmount>(AddDialogComponent, {data: bees})
+    //.....deleted 🐛🐛🐛🐛
+  }
+}
